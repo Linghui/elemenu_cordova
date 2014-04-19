0f09cd4a03abc2d379cd54ff7f36052df993b3ad
Kevin <xiaohuidexinge@163.com>
2014-04-19 18:28:09 +0800

    基本功能完毕

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -17,33 +17,91 @@
  * under the License.
  */
 var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
+	// Application Constructor
+	initialize : function() {
+		this.bindEvents();
+	},
+	// Bind Event Listeners
+	//
+	// Bind any events that are required on startup. Common events are:
+	// 'load', 'deviceready', 'offline', and 'online'.
+	bindEvents : function() {
+	}
 };
+
+function index_load_list() {
+	console.log('index_load_list');
+	
+	$("#afui").get(0).className = "bb";
+
+	var fandian_list = data_get_fandian_list();
+	if (fandian_list && fandian_list.length > 0) {
+		console.log('index_load_list list_page ' + fandian_list.length);
+		console.log('index_load_list list_page ' + JSON.stringify(fandian_list));
+
+		var list_content = '<ul id="fandian_list" class="list">';
+		for (var i = 0; i < fandian_list.length; i++) {
+			console.log("fandian_list name " + fandian_list[i].name);
+			list_content += "<li><a class='button' onclick='show_detail(" + fandian_list[i].fandian_id + ")'>";
+			list_content += fandian_list[i].name;
+			list_content += "</a></li>";
+		}
+		list_content += "</ul>";
+
+		console.log('list_content ' + list_content);
+		$('#main').html(list_content);
+
+	} else {
+		console.log('index_load_list null_page');
+
+		$('#main').html('<div width="100%" style="text-align:center"> <img class="center wucdbj" src="img/wucdbj.png"/> <h1>您没有录入任何菜单</h1> <a id="scan" onclick="scan()" class="button">开始扫描</a> </div>');
+
+	}
+
+}
+
+function show_detail(fandian_id) {
+	console.log("show_detail");
+	var fandian_info = data_get_fandian_by_id(fandian_id);
+
+	$('#fandian_name').html(fandian_info.name);
+
+	var menuStr = fandian_info.menu;
+	var menuObj = JSON.parse(menuStr);
+
+	var content = "";
+	for (var index = 0; index < menuObj.length; index++) {
+		content += "<li>";
+		content += menuObj[index].name;
+		content += menuObj[index].price;
+		content += "</li>";
+	}
+
+	$('#food_list').html(content);
+
+	$.ui.loadContent('detail', false, false, 'slide')
+}
+
+function add_test() {
+
+	var fandian_info_obj = {};
+	fandian_info_obj["fandian_id"] = "1";
+	fandian_info_obj["name"] = "测试饭店";
+	fandian_info_obj["cellphone"] = "12222333";
+	fandian_info_obj["address"] = "test";
+
+	var menuList = [];
+
+	var food = {};
+	food["food_id"] = "1";
+	food["fandian_id"] = "1";
+	food["name"] = "好吃的";
+	food["price"] = 11233;
+	menuList.push(food);
+
+	fandian_info_obj["menu"] = JSON.stringify(menuList);
+
+	data_insert(fandian_info_obj);
+
+}
+
---
+++ platforms/ios/www/js/scan.js
@@ -0,0 +1,81 @@
+function success(resultArray) {
+
+	var scanRes = resultArray[0];
+
+	if (/^elemenu/.test(scanRes)) {
+
+		var jsonStr = scanRes.substring(8);
+
+		var jsonObj = JSON.parse(jsonStr);
+
+		var requestRrl = getHttpUrl("fandian");
+
+		requestRrl += "?fandian_id=" + jsonObj.id;
+		console.log('requestRrl' + requestRrl);
+
+		$.ui.showMask();
+
+		$.ajax({
+			url : requestRrl,
+			success : function(data) {
+				
+
+				if(data){
+					var proto = JSON.parse(data);
+					if(proto && proto.length >= 1){
+						var fandian_info_str = proto[0].t;
+						var fandian_info_obj = JSON.parse(fandian_info_str);
+						if(data_insert(fandian_info_obj)){
+							index_load_list();
+						}
+					}	
+				}
+
+				$.ui.hideMask();
+			},
+			error : function(data) {
+				console.log('error' + data);
+				$.ui.hideMask();
+			}
+		});
+
+	} else {
+
+		alert('扫描结果: ' + scanRes);
+
+	}
+
+	// NOTE: Scandit SDK Phonegap Plugin Versions 1.* for iOS report
+	// the scanning result as a concatenated string.
+	// Starting with version 2.0.0, the Scandit SDK Phonegap
+	// Plugin for iOS reports the result as an array
+	// identical to the way the Scandit SDK plugin for Android reports results.
+
+	// If you are running the Scandit SDK Phonegap Plugin Version 1.* for iOS,
+	// use the following approach to generate a result array from the string result returned:
+	// resultArray = result.split("|");
+}
+
+function failure(error) {
+	alert("Failed: " + error);
+}
+
+function scan() {
+	// See below for all available options.
+	cordova.exec(success, failure, "ScanditSDK", "scan", ["MzySjscTEeOXwyMUN6u+RbQVe522gOFWNWgtMC0c4nM", {
+		"beep" : false,
+		"qr" : true,
+		"2DScanning" : true
+	}]);
+}
+
+function getHttpHead() {
+	return "http://www.jian-yin.com:3000/";
+}
+
+function getHttpUrl(request) {
+
+	return getHttpHead() + request;
+
+}
+
